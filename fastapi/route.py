57f95d84f17dcf52c0c65fb9d412ec5002c35fa8
Addyk-24 <addykatkar24@gmail.com>
2025-08-27 14:13:30 +0530

    Main Backend

--- fastapi/route.py
+++ fastapi/route.py
@@ -3,12 +3,21 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from pydantic import BaseModel
 
 from database.DatabaseManager import DatabaseManager
 from evalve.app import EvalveAgent
+from conversation_mem.convo_mem import ConversationMemory
 
 dm = DatabaseManager()
 ea = EvalveAgent()
+cm = ConversationMemory()
+
+# cm._generate_session_id()
+
+class ChatModel(BaseModel):
+    query : str
+    session_id : str
 
 app = FastAPI()
 
@@ -21,12 +30,24 @@ def get_all_startup():
     response = dm.get_all_startups()
     return response
 
-@app("/api/startups/{startup_id}")
+@app.get("/api/startups/{startup_id}")
 def get_specific_startup(startup_id:str):
     specific_profile = dm.get_startup_by_name_or_id(startup_id)
-    specific_profile_data = dm.get_startup_by_company_name(specific_profile)
 
     specific_profile_insights = ea.get_startup_insight(specific_profile)
 
-    return {f"{specific_profile}\n : {specific_profile_data} "}
+    return {f"{specific_profile.get('company_name')}" : {specific_profile},
+            "Insights" : {specific_profile_insights}
+            }
+
+@app.post("/api/startups/{startup_id}/chat")
+def specific_profile_chat(startup_id:str, req: ChatModel):
+
+    req.session_id = cm._generate_session_id()
+
+    response = ea.get_startup_chatbot(req.query,startup_id,req.session_id)
+
+    return {"response" : response}
+
+
 
